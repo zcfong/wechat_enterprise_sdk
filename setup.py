50f39280bbc93e4600652ef79e4e1057cacddaa3
justin.fang <justin.fang@01zc.com>
2016-04-07 11:03:57 +0800

    add setup

--- setup.py
+++ setup.py
@@ -1,3 +1,19 @@
 #!/usr/bin/env python
+# -*- coding:utf-8 -*-
+__author__ = 'justinfong'
 
-from distutils.core import setup
+from setuptools import setup, find_packages
+
+setup(
+    name='wechat-enterprise-sdk',
+    version='0.0.1',
+    keywords=('wechat-enterprise', 'wechat enterprise sdk', 'wechat enterprise sdk'),
+    description=u'微信企业平台Python开发包',
+    long_description="wechat enterprise sdk",
+    url='https://github.com/zcfong/wechat_enterprise_sdk',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=map(lambda x: x.replace('==', '>='), open("requirements.txt").readlines()),
+
+    test_suite='tests',
+)
